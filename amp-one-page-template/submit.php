bb2d91ffa634520db490a9977008d4751b331598
ceoapkakart@gmail.com <ceoapkakart@gmail.com>
2020-05-06 15:58:45 +0530

    6may

--- amp-one-page-template/submit.php
+++ amp-one-page-template/submit.php
@@ -1,24 +1,25 @@
 <?php
-  if(!empty($_POST)){
-  $name=$_POST['name'];
-  $email = $_POST['email'];
-  $phone = $_POST['my_tel'];
-  $enquiry = $_POST['enquiry'];
-  $formcontent=" From: $name \n Phone: $my_tel";
-  $recipient = "drabji71u@gmail.com";
-  $subject = $enquiry;
-  $mailheader = "From: $email \r\n";
-  mail($recipient, $subject, $formcontent, $mailheader) or die("Error!");
-  
-         $domain_url = (isset($_SERVER['HTTPS']) ? "https" : "http") . "://$_SERVER[HTTP_HOST]";
-          header("Content-type: application/json");
-          header("Access-Control-Allow-Credentials: true");
-          header("Access-Control-Allow-Origin: ". str_replace('.', '-','https://drsureshmondal.in') .".cdn.ampproject.org");
-          header("AMP-Access-Control-Allow-Source-Origin: " . $domain_url);
-          header("Access-Control-Expose-Headers: AMP-Access-Control-Allow-Source-Origin");
-          header("AMP-Redirect-To: https://drsureshmondal.in/contact.html");
-          header("Access-Control-Expose-Headers: AMP-Redirect-To, AMP-Access-Control-Allow-Source-Origin"); 
-          echo json_encode(array('name' => $name));
-          exit;
-  }
-  ?>
+       header("AMP-Same-Origin: true");
+       header("Alt-Svc: clear");
+       header("Content-type:application/json; charset=utf-8");
+       header("Strict-Transport-Security: max-age=63072000; includeSubDomains; preload");
+       header("Access-Control-Allow-Origin: https://drsureshmondal.in");
+       header("Access-Control-Allow-Credentials: true");
+       header("Cache-Control: public,max-age=3600,stale-while-revalidate=7200, no-cache");
+       header("AMP-Access-Control-Allow-Source-Origin: https://drsureshmondal.in");
+       header("AMP-Redirect-To: https://drsureshmondal.in/index.php");
+       header("Access-Control-Expose-Headers: AMP-Redirect-To, AMP-Access-Control-Allow-Source-Origin"); 
+      $name = $_POST['name'];
+$email = $_POST['email'];
+$message = $_POST['message'];
+$formcontent="From: $name \n Message: $message";
+$recipient = "info@drsureshmondal.in.com";
+$subject = "Contact Form";
+  echo json_encode(array('name' => $name));
+       
+$mailheader = "From: $email \r\n";
+mail($recipient, $subject, $formcontent, $mailheader) or die("Error!");
+
+        exit;
+
+?>
